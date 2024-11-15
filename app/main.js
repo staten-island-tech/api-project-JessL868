986a9a497ed13afa16eba834ec0df05dfc375a73
JessL868 <144480854+JessL868@users.noreply.github.com>
2024-11-15 14:01:31 -0500

    continue finding apis

--- app/main.js
+++ app/main.js
@@ -1,1 +1,21 @@
 import "./style.css";
+
+async function getData() {
+    try {
+        //returns a promise
+        const response = await fetch('');
+        //guard clause
+        if (response.status != 200) {
+            // throw new Error(response);
+        } else {
+            //convert promise to json
+            const data = await response.json();
+            console.log(data.data)
+            //this is unique to api
+            data.data.forEach((agent) => document.querySelector("div").insertAdjacentHTML ("afterbegin", `<h1>${}</h1>`));
+        }
+    } catch (error) {
+        alert("hey i couldnt find that agent");
+    }
+};
+getData();
