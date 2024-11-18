2acaa3fe0d7bcef706f1850f319f7d332e04ceb6
JessL868 <144480854+JessL868@users.noreply.github.com>
2024-11-18 14:02:27 -0500

    found api

--- app/main.js
+++ app/main.js
@@ -1,18 +1,20 @@
 import "./style.css";
 
+
+
 async function getData() {
     try {
         //returns a promise
-        const response = await fetch('');
+        const response = await fetch('https://gsi.fly.dev/characters');
         //guard clause
         if (response.status != 200) {
             // throw new Error(response);
         } else {
             //convert promise to json
             const data = await response.json();
-            console.log(data.data)
+            console.log(data.results)
             //this is unique to api
-            data.data.forEach((agent) => document.querySelector("div").insertAdjacentHTML ("afterbegin", `<h1>${}</h1>`));
+            data.results.forEach((result) => document.querySelector("div").insertAdjacentHTML("afterbegin", `<h1 class ="text-lg">${result.name}</h1>`));
         }
     } catch (error) {
         alert("hey i couldnt find that agent");
