6c4868efa82458158db080b31798607e3f1a220c
JessL868 <144480854+JessL868@users.noreply.github.com>
2024-11-20 14:02:58 -0500

    design

--- app/main.js
+++ app/main.js
@@ -15,7 +15,12 @@ async function getData() {
             console.log(data.results)
             //this is unique to api
             data.results.forEach((result) => document.querySelector("div").insertAdjacentHTML("beforeend", `
-                <h1 class ="text-lg">${result.name}</h1>`));
+        <div class="card ">
+            <h1>${result.name}</h1>
+            <h2>${result.rarity}</h2>
+            <h2>${result.vision}</h2>
+            <h2>${result.weapon}</h2>
+        </div>`));
         }
     } catch (error) {
         alert("hey i couldnt find that agent");
