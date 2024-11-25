a012e7c52d373f79be6b412b8dec682969935ef9
JessL868 <144480854+JessL868@users.noreply.github.com>
2024-11-25 14:02:21 -0500

    why doesnt my container prooperties

--- app/main.js
+++ app/main.js
@@ -14,16 +14,13 @@ async function getData() {
             const data = await response.json();
             console.log(data.results)
             //this is unique to api
-            data.results.forEach((result) => document.querySelector("div.container").insertAdjacentHTML("beforeend", `
-        <div class="card h-1/5 w-1/6 border-4 rounded-3xl flex flex-col mx-4 items-center justify-around bg-cover bg-white p-7">
+            data.results.forEach((result) => document.querySelector("div").insertAdjacentHTML("beforeend", `
+        <div class="card h-1/5 w-1/6 border-4 rounded-3xl flex flex-col mx-4 items-center justify-center bg-cover bg-white p-7">
             <h1>${result.name}</h1>
-            <h2>${result.rarity}</h2>
-            <h2>${result.vision}</h2>
-            <h2>${result.weapon}</h2>
         </div>`));
         }
     } catch (error) {
-        alert("hey i couldnt find that agent");
+        alert("hey i couldnt find that character");
     }
 };
 getData();
